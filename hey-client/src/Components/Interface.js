d8f59f1f043f2b7eaf523e765985414555d543f1
niiyazobug <niiyaz5555@gmail.com>
2022-06-08 15:46:43 +0600

    ui created

--- hey-client/src/Components/Interface.js
+++ hey-client/src/Components/Interface.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io, { Socket } from 'socket.io-client'
 import TextField from '@material-ui/core/TextField'
 
@@ -7,12 +7,47 @@ const socket = io.connect('http://localhost:4000/')
 
 const Interface = () => {
 
-const [state, setState]=useState({message: '', name: ''})
+    const [state, setState] = useState({ message: '', name: '' })
+
+    const [chat, setChat] = useState([])
+
+
 
 
     return (
         <div>
-            
+
+            <form onSubmit={onMessageSubmit}>
+
+                <div className="name-field">
+                    <TextField
+                        name='name'
+                        onChange={e => onTextChange(e)}
+                        value={state.name}
+                        label='Name'
+                    />
+                </div>
+
+                <div>
+                    <TextField
+                        name='message'
+                        onChange={e => onTextChange(e)}
+                        value={state.message}
+                        id='outliend-multiline-static'
+                        variant='outlined'
+                        label='Message'
+                    />
+                </div>
+
+                <button>Send</button>
+
+            </form>
+
+            <div className="render-chat">
+                <h1>Chat Log</h1>
+                {renderChat()}
+            </div>
+
         </div>
     );
 };
